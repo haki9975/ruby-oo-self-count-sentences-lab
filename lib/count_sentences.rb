a1c2dade6bffa165f2590a5e736fa2f57f2af6d2
haki9975 <elias.robert.hakim@gmail.com>
2021-02-12 01:00:26 -0500

    commit tree e089a9c61983f0044f4889f5e57350450815f8d5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+      counter = 0
+      self.split.each do |word| 
+        if word.end_with?(".")
+           counter +=1
+        elsif word.end_with?("?")
+          counter+=1 
+        elsif word.end_with?("!")
+          counter += 1
+        end
+      end
+      counter
   end
 end
+
+
+
+
+
+  
